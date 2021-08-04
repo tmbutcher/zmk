4ddec7b5a3ebf729b179335a3151e37a67680022
tmbutcher <tmbutcher@gmail.com>
2021-08-04 13:19:51 -0500

    Added bat script to automate build, backup, & flash

---
+++ app/Space Invader Script.bat
@@ -0,0 +1,51 @@
+@ECHO OFF
+
+choice /C YNQ /T 5 /D Y /M "Build firmware? [Y]es | [N]o | [Q]uit" /N
+if %ERRORLEVEL% EQU 1 goto buildFirmware
+if %ERRORLEVEL% EQU 2 goto copyFirmware
+if %ERRORLEVEL% EQU 3 goto end
+
+:buildFirmware
+d:
+cd D:\Win\Users\Tom\Documents\Git\zmk\app
+west build -b adafruit_feather_nrf52840 -- -DSHIELD=space_invader
+if %ERRORLEVEL% neq 0 goto pristineBuild
+if %ERRORLEVEL% EQU 0 goto copyFirmware
+
+:pristineBuild
+west build -p -b adafruit_feather_nrf52840 -- -DSHIELD=space_invader
+if %ERRORLEVEL% neq 0 goto copyFirmware
+if %ERRORLEVEL% EQU 0 goto end
+
+:copyFirmware
+cd D:\Win\Users\Tom\Documents\Git\zmk\app\backup_firmware
+del zmk.uf2.bk5
+rename zmk.uf2.bk4 zmk.uf2.bk5
+rename zmk.uf2.bk3 zmk.uf2.bk4
+rename zmk.uf2.bk2 zmk.uf2.bk3
+rename zmk.uf2.bk1 zmk.uf2.bk2
+rename zmk.uf2 zmk.uf2.bk1
+cd ..
+copy /y build\zephyr\zmk.uf2 backup_firmware\zmk.uf2
+
+Set "Drv="
+For /F Skip^=1 %%A In (
+    'WMIC LogicalDisk Where "VolumeName='FTHR840BOOT'" Get DeviceID 2^>NUL'
+)Do For /F Tokens^=* %%B In ("%%A")Do Set "Drv=%%B"
+If Not Defined Drv goto dfuPrompt
+@REM Echo Your Drive Letter is %Drv%
+@REM Timeout 3 >NUL
+copy D:\Win\Users\Tom\Documents\Git\zmk\app\build\zephyr\zmk.uf2 %Drv%
+if %ERRORLEVEL% EQU 0 goto end
+if %ERRORLEVEL% neq 0 goto dfuPrompt
+
+:dfuPrompt
+powershell "[console]::beep(700,300)"
+powershell "[console]::beep(700,300)"
+choice /C ONA /T 10 /D A /M "Put Space Invader in DFU mode. [O]kay | [N]o | [A]lready there" /N
+if %ERRORLEVEL% EQU 3 goto copyFirmware
+if %ERRORLEVEL% EQU 2 goto end
+timeout 10 /nobreak
+if %ERRORLEVEL% EQU 1 goto copyFirmware
+
+:end
