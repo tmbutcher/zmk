a20cf6feb6bf5750988e6041888ba0b982668e07
tmbutcher <tmbutcher@gmail.com>
2021-08-05 00:28:55 -0500

    Minor bat script changes

--- app/Space Invader Script.bat
+++ app/Space Invader Script.bat
@@ -9,16 +9,18 @@ if %ERRORLEVEL% EQU 3 goto end
 d:
 cd D:\Win\Users\Tom\Documents\Git\zmk\app
 west build -b adafruit_feather_nrf52840 -- -DSHIELD=space_invader
-if %ERRORLEVEL% neq 0 goto pristineBuild
 if %ERRORLEVEL% EQU 0 goto copyFirmware
+if %ERRORLEVEL% neq 0 goto hold
+
 
 :pristineBuild
 west build -p -b adafruit_feather_nrf52840 -- -DSHIELD=space_invader
-if %ERRORLEVEL% neq 0 goto copyFirmware
-if %ERRORLEVEL% EQU 0 goto end
+if %ERRORLEVEL% EQU 0 goto copyFirmware
+if %ERRORLEVEL% neq 0 goto hold
 
 :copyFirmware
 cd D:\Win\Users\Tom\Documents\Git\zmk\app\backup_firmware
+echo.
 del zmk.uf2.bk5
 rename zmk.uf2.bk4 zmk.uf2.bk5
 rename zmk.uf2.bk3 zmk.uf2.bk4
@@ -42,10 +44,18 @@ if %ERRORLEVEL% neq 0 goto dfuPrompt
 :dfuPrompt
 powershell "[console]::beep(700,300)"
 powershell "[console]::beep(700,300)"
+
+echo.
 choice /C ONA /T 10 /D A /M "Put Space Invader in DFU mode. [O]kay | [N]o | [A]lready there" /N
 if %ERRORLEVEL% EQU 3 goto copyFirmware
 if %ERRORLEVEL% EQU 2 goto end
 timeout 10 /nobreak
 if %ERRORLEVEL% EQU 1 goto copyFirmware
+
+:hold
+echo.
+choice /C RX /T 9999 /D X /M "Well that didn't work. [R]estart or e[X]it?" /N
+if %ERRORLEVEL% EQU 1 goto buildFirmware
+if %ERRORLEVEL% EQU 2 goto end
 
 :end
