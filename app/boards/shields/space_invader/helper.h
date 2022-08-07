70961a9fdba92901d0a7b4a747787748f8d61e78
tmbutcher <tmbutcher@gmail.com>
2022-08-07 16:24:43 -0500

    Moving other behaviors to a separate file

--- app/boards/shields/space_invader/helper.h
+++ app/boards/shields/space_invader/helper.h
@@ -9,29 +9,7 @@
 
 #define ZMK_HELPER_STRINGIFY(x) #x
 
-/* ZMK_BEHAVIOR */
-
-#define ZMK_BEHAVIOR_CORE_caps_word   compatible = "zmk,behavior-caps-word";  #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_hold_tap    compatible = "zmk,behavior-hold-tap";   #binding-cells = <2>
-#define ZMK_BEHAVIOR_CORE_key_repeat  compatible = "zmk,behavior-key-repeat"; #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_macro       compatible = "zmk,behavior-macro";      #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_mod_morph   compatible = "zmk,behavior-mod-morph";  #binding-cells = <0>
-#define ZMK_BEHAVIOR_CORE_sticky_key  compatible = "zmk,behavior-sticky-key"; #binding-cells = <1>
-#define ZMK_BEHAVIOR_CORE_tap_dance   compatible = "zmk,behavior-tap-dance";  #binding-cells = <0>
-
-#define ZMK_BEHAVIOR(name, type, ...) \
-    / { \
-        behaviors { \
-            name: name { \
-                label = ZMK_HELPER_STRINGIFY(ZB_ ## name); \
-                ZMK_BEHAVIOR_CORE_ ## type; \
-                __VA_ARGS__ \
-            }; \
-        }; \
-    };
-
 /* ZMK_LAYER */
-
 #define ZMK_LAYER(name, layout) \
     / { \
         keymap { \
@@ -42,45 +20,8 @@
         }; \
     };
 
-/* ZMK_COMBOS */
-
-#define ALL 0xff
-#if !defined COMBO_TERM
-    #define COMBO_TERM 30
-#endif
-
-#define ZMK_COMBO(name, combo_bindings, keypos, combo_layers) \
-    / { \
-        combos { \
-            compatible = "zmk,combos"; \
-            combo_ ## name { \
-                timeout-ms = <COMBO_TERM>; \
-                bindings = <combo_bindings>; \
-                key-positions = <keypos>; \
-                layers = <combo_layers>; \
-            }; \
-        }; \
-    };
-
-#if !defined COMBO_ADD_ON
-    #define COMBO_ADD_ON
-#endif
-#define ZMK_COMBO_ADV(name, combo_bindings, keypos, combo_layers, combo_timeout) \
-    / { \
-        combos { \
-            compatible = "zmk,combos"; \
-            combo_ ## name { \
-                timeout-ms = <combo_timeout>; \
-                bindings = <combo_bindings>; \
-                key-positions = <keypos>; \
-                layers = <combo_layers>; \
-                COMBO_ADD_ON \
-            }; \
-        }; \
-    };
 
 /* ZMK_CONDITIONAL_LAYER */
-
 #define ZMK_CONDITIONAL_LAYER(if_layers, then_layer) \
     / { \
         conditional_layers { \
@@ -92,8 +33,8 @@
         }; \
     };
 
-/* ZMK_UNICODE */
 
+/* ZMK_UNICODE */
 #if !defined OS_UNICODE_LEAD
     #if HOST_OS == 2
         #define OS_UNICODE_LEAD &macro_press &kp LALT      // macOS
@@ -149,4 +90,3 @@
     UC_MACRO(name ## _lower, &kp L0 &kp L1 &kp L2 &kp L3) \
     UC_MACRO(name ## _upper, &kp U0 &kp U1 &kp U2 &kp U3) \
     UC_MODMORPH(name, &name ## _lower, &name ## _upper)
-
