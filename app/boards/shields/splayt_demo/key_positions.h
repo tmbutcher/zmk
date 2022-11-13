929e413335ee7d947ae0f7000a5935b302540a35
tmbutcher <tmbutcher@gmail.com>
2022-11-13 01:00:19 -0600

    Add Choc Splayt Demo

---
+++ app/boards/shields/splayt_demo/key_positions.h
@@ -0,0 +1,69 @@
+/*  ------------------------------------------------ KEY POSITIONS----------------------------------------------------								
+																				    
+	╭────────────────────╮          ╭────────────────────╮   ╭────────────────────╮          ╭────────────────────╮
+	│  0   1   2   3   4 │          │  5   6   7   8   9 │   │ LT1 LT2 LT3 LT4 LT5|          | RT1 RT2 RT3 RT4 RT5│
+	│ 10  11  12  13  14 │   ╭──╮   │ 15  16  17  18  19 │   │ LM1 LM2 LM3 LM4 LM5│   ╭──╮   │ RM1 RM2 RM3 RM4 RM5│
+	│ 20  21  22  23  24 ╰───╮25╭───╯ 26  27  28  29  30 │   │ LB1 LB2 LB3 LB4 LB5╰───╮EC╭───╯ RB1 RB2 RB3 RB4 RB5│
+	│ 31 ╭──╮ 32  33  34  35 │‾‾│ 36  37  38  39 ╭──╮ 40 │   │ LH1╭──╮ LH2 LH3 LH4 LH5│‾‾│ RH1 RH2 RH3 RH4╭──╮ RH5│
+	╰────╯  ╰────────────────╯  ╰────────────────╯  ╰────╯   ╰────╯  ╰────────────────╯  ╰────────────────╯  ╰────╯
+	                        ╭────╮		                                           	 ╭────╮		   
+	                    ╭───╯ 41 ╰───╮                                           ╭───╯ JU ╰───╮
+	                    │ 42  43  44 │                                           │ JL  JC  JR │
+	                    ╰───╮ 45 ╭───╯                                           ╰───╮ JR ╭───╯
+	                        ╰────╯                                                   ╰────╯						    */
+
+#pragma once
+
+#define LT1  0	// left-top row
+#define LT2  1
+#define LT3  2
+#define LT4  3
+#define LT5  4
+
+#define RT1  5  // right-top row
+#define RT2  6
+#define RT3  7
+#define RT4  8
+#define RT5  9
+
+#define LM1 10  // left-middle row
+#define LM2 11
+#define LM3 12
+#define LM4 13
+#define LM5 14
+
+#define RM1 15  // right-middle row
+#define RM2 16
+#define RM3 17
+#define RM4 18
+#define RM5 19
+
+#define LB1 20  // left-bottom row
+#define LB2 21
+#define LB3 22
+#define LB4 23
+#define LB5 24
+
+#define RB1 26  // right-bottom row
+#define RB2 27
+#define RB3 28
+#define RB4 29
+#define RB5 30
+
+#define LH1 31  // left thumb keys
+#define LH2 32
+#define LH3 33
+#define LH4 34
+#define LH5 35
+
+#define RH1 36  // right thumb keys
+#define RH2 37
+#define RH3 38
+#define RH4 39
+#define RH5 40
+
+#define EN	25  // encoder and joystick
+#define JU	41
+#define JL	42
+#define JC	43
+#define JD	44
